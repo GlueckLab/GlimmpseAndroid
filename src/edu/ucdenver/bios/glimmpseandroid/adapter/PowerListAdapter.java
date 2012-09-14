c26dfbf8c76ed37553acd05b11ebf03755087849
Uttara Sakhadeo <uttarasakhadeo@gmail.com>
2012-09-14 22:20:07 +0000

    removed a bug

--- src/edu/ucdenver/bios/glimmpseandroid/adapter/PowerListAdapter.java
+++ src/edu/ucdenver/bios/glimmpseandroid/adapter/PowerListAdapter.java
@@ -20,6 +20,8 @@
  */
 package edu.ucdenver.bios.glimmpseandroid.adapter;
 
+import java.util.List;
+
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
@@ -27,112 +29,129 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import edu.ucdenver.bios.glimmpseandroid.R;
 import edu.ucdenver.bios.glimmpseandroid.application.StuyDesignContext;
+import edu.ucdenver.bios.webservice.common.domain.NominalPower;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class PowerListAdapter.
+ * @author Uttara Sakhadeo
+ */
 public class PowerListAdapter extends BaseAdapter{
+    
+    /** The m layout inflater. */
     private LayoutInflater mLayoutInflater;
-    private static int count;
+    
+    private List<NominalPower> list;
+        
+    /** The power. */
     static double power;
     //static ViewHolder holder;
+    /** The power text. */
     static TextView powerText;
-    private static Drawable img;
-    private static StuyDesignContext globalVariables = StuyDesignContext.getInstance();
     
+    /** The img. */
+    private static Drawable img;
+    
+    /** The global variables. */
+    private static StuyDesignContext globalVariables;
+        
+    /**
+     * Instantiates a new power list adapter.
+     *
+     * @param context the context
+     * @param value the value
+     */
     public PowerListAdapter(Context context, Double value){
         
         //get the layout inflater
         mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        int size = globalVariables.getPowerListSize();
-        if(size == 0 && count > 0)
-            count=0;
-        else if(count > size)
-            count = size;
-        if(value != null && count < 5) { 
+        globalVariables = StuyDesignContext.getInstance();
+        list = globalVariables.getStudyDesign().getNominalPowerList();               
+        if(value != null ) {             
             if(value == -1.0) {
                 globalVariables.clearPowerList();
-                value = null;
-                count = 0;
+                value = null;                
             }
-            else {                
-                //System.out.println("Count : "+count+" list size : "+size); 
-                count++;
+            else if (getCount() < 5) {                                
                 power = value;
-                globalVariables.setPower(power, count-1);
-                //System.out.println("Count : "+count+" Power : "+globalVariables.getPower(count-1));
+                globalVariables.setPower(power, getCount());                
             }
         }
+        
         img = context.getResources().getDrawable( R.drawable.clear_button);
-        img.setBounds(0,0,30,30); 
-        //notifyDataSetChanged();
+        img.setBounds(0,0,30,30);         
     }
     
+    /* (non-Javadoc)
+     * @see android.widget.Adapter#getItem(int)
+     */
     public Object getItem(int i) {
-        return null;
+        return list.get(i).getValue();
     }
  
     
         //get the position id of the item from the list
-    public long getItemId(int i) {
-        return 0;
+    /* (non-Javadoc)
+         * @see android.widget.Adapter#getItemId(int)
+         */
+        public long getItemId(int i) {
+        return i;
     }
 
+    /* (non-Javadoc)
+     * @see android.widget.Adapter#getCount()
+     */
     public int getCount() {
         // TODO Auto-generated method stub
-        return count;
+        return globalVariables.getPowerListSize();
     }
 
+    /* (non-Javadoc)
+     * @see android.widget.Adapter#getView(int, android.view.View, android.view.ViewGroup)
+     */
     public View getView(final int position, View view, ViewGroup parent) {      
-        ViewHolder holder;      
-        //check to see if the reused view is null or not, if is not null then reuse it
-        //if (view == null) {
+        ViewHolder holder;         
+        if(view == null){
+        
             view = mLayoutInflater.inflate(R.layout.design_power_item, null);
             
             holder = new ViewHolder();
             holder.textLine = (TextView) view.findViewById(R.id.list_item_textView_power);            
+           
+            view.setTag(holder);
+                                   
+        }
+        else {
+            holder = (ViewHolder) view.getTag();
+        }
+        holder.textLine.setText(String.valueOf(globalVariables.getPower(position))); 
+        holder.textLine.setCompoundDrawables(null, null, img, null);
+        holder.textLine.setOnTouchListener(new OnTouchListener() {
             
-            //System.out.println("position : "+position);
-            holder.textLine.setText(String.valueOf(globalVariables.getPower(position))); 
-            holder.textLine.setCompoundDrawables(null, null, img, null);
-            holder.textLine.setOnTouchListener(new OnTouchListener() {
-                
-                public boolean onTouch(View v, MotionEvent arg1) {
-                    powerText = (TextView) v.findViewById(R.id.list_item_textView_power);
-                    if (arg1.getX() > powerText.getWidth()
-                    - img.getIntrinsicWidth() - 10) {
-                        globalVariables.clearPower(position);                                        
-                        //v.setLayoutParams(new RelativeLayout.LayoutParams(1, 1)); 
-                        View parentView = (LinearLayout)v.getParent().getParent();
-                        parentView.setLayoutParams(new AbsListView.LayoutParams(1,1));                        
-                        //count--;
-                    }
-                    return false;
+            public boolean onTouch(View v, MotionEvent arg1) {
+                powerText = (TextView) v.findViewById(R.id.list_item_textView_power);
+                if (arg1.getX() > powerText.getWidth()
+                - img.getIntrinsicWidth() - 10) {
+                    globalVariables.clearPower(position);                                                            
+                    notifyDataSetChanged();
                 }
-        
-            });
-            
-            /*holder.textLine.setOnFocusChangeListener(new OnFocusChangeListener() {
-
-                public void onFocusChange(View v, boolean hasFocus) {
-                    System.out.println("calling floodVariance()");                    
-                    if (!hasFocus){
-                        final int position = v.getId();
-                        final TextView text = (TextView) v;
-                        myItems.get(position).caption = text.getText().toString();
-                    }      
-                }
-            });*/
-            
-                        
-        //}
+                return false;
+            }
+    
+        });
         return view;
     }
     
+    /**
+     * The Class ViewHolder.
+     */
     static class ViewHolder {
+        
+        /** The text line. */
         TextView textLine;        
     }
 }
