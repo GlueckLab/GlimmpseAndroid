b9907fbaf28353458b97a4edd5e028e53431205e
Uttara Sakhadeo <uttarasakhadeo@gmail.com>
2012-09-14 22:39:02 +0000

    fixed a bug

--- src/edu/ucdenver/bios/glimmpseandroid/adapter/SampleSizeAdapter.java
+++ src/edu/ucdenver/bios/glimmpseandroid/adapter/SampleSizeAdapter.java
@@ -20,6 +20,8 @@
  */
 package edu.ucdenver.bios.glimmpseandroid.adapter;
 
+import java.util.List;
+
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
@@ -33,109 +35,125 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import edu.ucdenver.bios.glimmpseandroid.R;
 import edu.ucdenver.bios.glimmpseandroid.application.StuyDesignContext;
+import edu.ucdenver.bios.webservice.common.domain.SampleSize;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class SampleSizeAdapter.
+ * @author Uttara Sakhadeo
+ */
 public class SampleSizeAdapter extends BaseAdapter{
-    private LayoutInflater mLayoutInflater;
-    private static int count;
-    static int sampleSize;
-    //static ViewHolder holder;
-    static TextView sampleSizeText;
-    private static Drawable img;
-    private static StuyDesignContext globalVariables = StuyDesignContext.getInstance();
     
+    /** The m layout inflater. */
+    private LayoutInflater mLayoutInflater;
+    
+    private static List<SampleSize> list;
+    /** The sample size. */
+    static int sampleSize;
+    
+    /** The sample size text. */
+    static TextView sampleSizeText;
+    
+    /** The img. */
+    private static Drawable img;
+    
+    /** The global variables. */
+    private static StuyDesignContext globalVariables;
+    
+    /**
+     * Instantiates a new sample size adapter.
+     *
+     * @param context the context
+     * @param value the value
+     */
     public SampleSizeAdapter(Context context, Integer value){
         
         //get the layout inflater
         mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        Integer size = globalVariables.getSampleSizeListSize();
-        System.out.println("sampleSizeAdapter size : "+size);
-        if(size == null)
-            count = 0;
-        else if(count > 0 && size == 0)
-            count=0;
-        else if(count > size)
-            count = size;
-        if(value != null && count < 5) { 
+        globalVariables = StuyDesignContext.getInstance();
+        list = globalVariables.getStudyDesign().getSampleSizeList();
+        if(value != null) { 
             if(value == -1) {
                 globalVariables.clearSampleSizeList();
-                value = null;
-                count = 0;
+                value = null;                
             }
-            else {                
-                System.out.println("Count : "+count+" list size : "+size); 
-                count++;
+            else if (getCount() < 5) {                
                 sampleSize = value;
-                globalVariables.setSampleSize(sampleSize, count-1);
-                System.out.println("SampleSize : "+sampleSize+" Count : "+count+" SampleSize : "+globalVariables.getSampleSize(count-1));
+                globalVariables.setSampleSize(sampleSize, getCount());                
             }
         }
         img = context.getResources().getDrawable( R.drawable.clear_button);
-        img.setBounds(0,0,30,30); 
-        //notifyDataSetChanged();
+        img.setBounds(0,0,30,30);         
     }
     
+    /* (non-Javadoc)
+     * @see android.widget.Adapter#getItem(int)
+     */
     public Object getItem(int i) {
-        return null;
+        return list.get(i).getValue();
     }
  
     
         //get the position id of the item from the list
-    public long getItemId(int i) {
-        return 0;
+    /* (non-Javadoc)
+         * @see android.widget.Adapter#getItemId(int)
+         */
+        public long getItemId(int i) {
+        return i;
     }
 
+    /* (non-Javadoc)
+     * @see android.widget.Adapter#getCount()
+     */
     public int getCount() {
         // TODO Auto-generated method stub
-        return count;
+        return globalVariables.getSampleSizeListSize();
     }
 
+    /* (non-Javadoc)
+     * @see android.widget.Adapter#getView(int, android.view.View, android.view.ViewGroup)
+     */
     public View getView(final int position, View view, ViewGroup parent) {      
         ViewHolder holder;      
         //check to see if the reused view is null or not, if is not null then reuse it
-        //if (view == null) {
+        if (view == null) {
             view = mLayoutInflater.inflate(R.layout.design_sample_size_item, null);
             
             holder = new ViewHolder();
             holder.textLine = (TextView) view.findViewById(R.id.list_item_textView_sample_size);            
             
-            System.out.println("position : "+position);
-            holder.textLine.setText(String.valueOf(globalVariables.getSampleSize(position))); 
-            holder.textLine.setCompoundDrawables(null, null, img, null);
-            holder.textLine.setOnTouchListener(new OnTouchListener() {
-                
-                public boolean onTouch(View v, MotionEvent arg1) {
-                    sampleSizeText = (TextView) v.findViewById(R.id.list_item_textView_sample_size);
-                    if (arg1.getX() > sampleSizeText.getWidth()
-                    - img.getIntrinsicWidth() - 10) {
-                        globalVariables.clearSampleSize(position);                                        
-                        //v.setLayoutParams(new RelativeLayout.LayoutParams(1, 1)); 
-                        View parentView = (LinearLayout)v.getParent().getParent();
-                        parentView.setLayoutParams(new AbsListView.LayoutParams(1,1));                        
-                        //count--;
-                    }
-                    return false;
-                }
+            view.setTag(holder);
+            
+        }
+        else {
+            holder = (ViewHolder) view.getTag();
+        }
         
-            });
+        holder.textLine.setText(String.valueOf(globalVariables.getSampleSize(position))); 
+        holder.textLine.setCompoundDrawables(null, null, img, null);
+        holder.textLine.setOnTouchListener(new OnTouchListener() {
             
-            /*holder.textLine.setOnFocusChangeListener(new OnFocusChangeListener() {
-
-                public void onFocusChange(View v, boolean hasFocus) {
-                    System.out.println("calling floodVariance()");                    
-                    if (!hasFocus){
-                        final int position = v.getId();
-                        final TextView text = (TextView) v;
-                        myItems.get(position).caption = text.getText().toString();
-                    }      
+            public boolean onTouch(View v, MotionEvent arg1) {
+                sampleSizeText = (TextView) v.findViewById(R.id.list_item_textView_sample_size);
+                if (arg1.getX() > sampleSizeText.getWidth()
+                - img.getIntrinsicWidth() - 10) {
+                    globalVariables.clearSampleSize(position);                                        
+                    notifyDataSetChanged();
                 }
-            });*/
-            
-                        
-        //}
+                return false;
+            }
+    
+        });
+        
         return view;
     }
     
+    /**
+     * The Class ViewHolder.
+     */
     static class ViewHolder {
+        
+        /** The text line. */
         TextView textLine;        
     }
 }
