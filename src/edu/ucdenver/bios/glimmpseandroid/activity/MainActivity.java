89b637d5a17c67d644cfbce3cf85a0fa2ba5bbee
Uttara Sakhadeo <uttarasakhadeo@gmail.com>
2012-11-19 22:34:58 +0000

    Code cleanup

--- src/edu/ucdenver/bios/glimmpseandroid/activity/MainActivity.java
+++ src/edu/ucdenver/bios/glimmpseandroid/activity/MainActivity.java
@@ -24,7 +24,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.Debug;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
